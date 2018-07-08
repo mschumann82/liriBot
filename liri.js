1b8c3fb1b17b31f752fc61c6e8c03519d2763b0a
Michael Schumann <schumann82@gmail.com>
2018-07-08 13:51:03 -0700

    keys loaded

--- liri.js
+++ liri.js
@@ -1,5 +1,38 @@
 require("dotenv").config();
-import "key.js";
+require("./keys");
+const Twitter = require('twitter');
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+
+const commands = process.argv[2];
+
+switch (commands) {
+    case "my-tweets":
+      myTweets();    //---- make functions for each command.
+      break;
+    
+    case "spotify-this-song":
+      //deposit();
+      break;
+    
+    case "movie-this":
+      //withdraw();
+      break;
+    
+    case "do-what-it-says":
+      //lotto();
+      break;
+    }
+
+    function myTweets() {
+               
+          var params = {screen_name: 'nodejs'};
+          client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+              console.log(tweets);
+            }
+            console.log(JSON.stringify(response, null, 2));
+          });
+    }
