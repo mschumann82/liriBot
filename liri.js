a71a264435bb1008455d4c801e6b5b160cd13c0d
Michael Schumann <schumann82@gmail.com>
2018-07-11 16:34:18 -0700

    omdb request

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var keys = require("./keys");
 var fs = require('fs');
 var Twitter = require('twitter');
-require("request");
+var request = require("request");
 var Spotify = require('node-spotify-api');
 
 
@@ -13,6 +13,7 @@ var client = new Twitter(keys.twitter);
 
 const commands = process.argv[2];
 const search = process.argv[3];
+const nodeArgs = process.argv;
 
 switch (commands) {
     case "my-tweets":
@@ -24,7 +25,7 @@ switch (commands) {
       break;
     
     case "movie-this":
-      //();
+      movie();
       break;
     
     case "do-what-it-says":
@@ -63,7 +64,35 @@ switch (commands) {
                 console.log(data.tracks.items[0].preview_url);
             
             
-         
-            // Do something with 'data'
+           
         });
+    }
+
+    function movie() {
+    //   let movieName = "";
+    //   for (var i = 2; i < nodeArgs.length; i++) {
+    //     if (i > 2 && i < nodeArgs.length) {
+    //     movieName = movieName + "+" + nodeArgs[i];
+    //     }
+    //     else {
+    //         movieName += nodeArgs[i];
+    //     }
+    // }
+    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+
+    
+
+    request (queryUrl, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+          
+          console.log(JSON.parse(body).Title);
+          console.log(JSON.parse(body).Year);
+          console.log(JSON.parse(body).imdbRating);
+          console.log(JSON.parse(body).Ratings[0]);
+          console.log(JSON.parse(body).Country);
+          console.log(JSON.parse(body).Language);
+          console.log(JSON.parse(body).Plot);
+          console.log(JSON.parse(body).Actors);
+      }
+    })
     }
